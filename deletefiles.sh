deeb42962476585eee8f9cfdcb4ae66759266e3f
Yang Su <suyang1+services@gmail.com>
2011-11-21 23:22:45 -0500

    added deletefiles script for testing

---
+++ deletefiles.sh
@@ -0,0 +1,14 @@
+rm TestPrograms/test11.tig
+rm TestPrograms/test12.tig
+rm TestPrograms/test14.tig
+rm TestPrograms/test15.tig
+rm TestPrograms/test18.tig
+rm TestPrograms/test19.tig
+rm TestPrograms/test21.tig
+rm TestPrograms/test34.tig
+rm TestPrograms/test4.tig
+rm TestPrograms/test44.tig
+rm TestPrograms/test6.tig
+rm TestPrograms/test7.tig
+rm TestPrograms/test8.tig
+rm TestPrograms/test9.tig
